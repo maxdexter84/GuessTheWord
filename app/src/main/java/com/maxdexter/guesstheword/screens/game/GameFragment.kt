b08bc4fbdcf866b16c37fbf29cd4883816066934
maxdexter <maxdexter@bk.ru>
2020-08-24 09:46:03 +0300

    Создал GameFragment, ScoreFragment, TitleFragment

---
+++ app/src/main/java/com/maxdexter/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,18 @@
+package com.maxdexter.guesstheword.screens.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GameFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/maxdexter/guesstheword/screens/score/ScoreFragment.kt
@@ -0,0 +1,19 @@
+package com.maxdexter.guesstheword.screens.score
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ScoreFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
